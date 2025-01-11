3ae22bcc133aa0d248708f88df070c46b00fae87
masamasaowl <aryancheers@gmail.com>
2025-01-11 22:05:01 +0530

    major style improvements and some minor bug fixes

--- models/listing.js
+++ models/listing.js
@@ -10,12 +10,18 @@ const listingSchema = new Schema({
         type : String,
     },
     image : {
-        type : Object,
-        
-        default : "https://plus.unsplash.com/premium_photo-1661963188432-5de8a11f21a7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        filename: {
+            type: String,
+            required: true,
+            default: "listing image"
+        },
+        url: {
+            type: String,
+            default : "https://plus.unsplash.com/premium_photo-1661963188432-5de8a11f21a7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
-        // for an empty image
-        set : (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1661963188432-5de8a11f21a7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+            // for an empty image
+            set : (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1661963188432-5de8a11f21a7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+        },  
     },
     price : {
         type : Number,
