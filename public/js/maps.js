b83e365af0874b77a42939f7dec27b36da2a0df4
masamasaowl <aryancheers@gmail.com>
2025-03-01 21:26:18 +0530

    the google maps API is working with geocoding and markers inside show.ejs

---
+++ public/js/maps.js
@@ -0,0 +1,29 @@
+console.log("map route is functional");
+
+// Initialize and add the map
+let map;
+
+async function initMap(latitude, longitude) {
+    //  destination 
+    const position = { lat: latitude, lng: longitude};
+  
+    // Request needed libraries.
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  
+    // The map   
+    map = new Map(document.getElementById("map"), {
+      zoom: 18,
+      center: position,
+      mapId: "DEMO_MAP_ID",
+    });
+  
+    // The marker
+    const marker = new AdvancedMarkerElement({
+      map: map,
+      position: position,
+      title: "Destination",
+    });
+}
+
+// initMap(listingCoordinates[1], listingCoordinates[0]);
